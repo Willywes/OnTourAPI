8c0dd7ab719a5c3f92d9260849634f3c8834c7ad
willywes <willywes_69@hotmail.com>
2018-06-13 15:19:03 -0400

    SPS

--- app/Contacto.php
+++ app/Contacto.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contacto extends Model
 {
-    //
+    protected $table = 'contactos';
+    protected $fillable = [
+        'id',
+        'version',
+        'nombres',
+        'email',
+        'comentarios',
+        'created_at',
+        'updated_at'
+    ];
 }
